731cc99781f37f7fe075597e73e49ca93704c624
Prophecyuntold <Aminurr999@gmail.com>
2023-12-18 18:52:27 +0000

    build form components, for image selection and popover

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationPage = () => {
-  return <div>OrganizationPage</div>;
+  return (
+    <div className="w-full mb-20">
+      <Info isPro={true} />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationPage;
