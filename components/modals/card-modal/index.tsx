72a7d8bd3949745ddeaa69e5e74c6e402f5f597c
Prophecyuntold <Aminurr999@gmail.com>
2024-03-23 14:55:21 +0000

    build card modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -5,7 +5,7 @@ import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
 import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
-import Header from "./header";
+import { Header } from "./header";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -17,24 +17,28 @@ export const CardModal = () => {
     queryFn: () => fetcher(`/api/cards/${id}`),
   });
 
-
- return (
-   <Dialog open={isOpen} onOpenChange={onClose}>
-     <DialogContent>
-       {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
-       <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
-         <div className="col-span-3">
-           <div className="w-full space-y-6">
-             {!cardData ? (
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {/* {!cardData ? (
                <Description.Skeleton />
              ) : (
                <Description data={cardData} />
              )}
-           </div>
-         </div>
-         {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
-       </div>
-     </DialogContent>
-   </Dialog>
- );
+             {!auditLogsData ? (
+               <Activity.Skeleton />
+             ) : (
+               <Activity items={auditLogsData} />
+             )} */}
+            </div>
+          </div>
+          {/* {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />} */}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 };
