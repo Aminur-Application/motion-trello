fb04ca748c689330f7045555d96bf97f4a5471dd
Prophecyuntold <Aminurr999@gmail.com>
2024-03-23 14:38:34 +0000

    use tanstack to fetch data from api

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,3 +1,4 @@
+import { QueryProvider } from "@/components/modals/card-modal/query-provider";
 import ModalProvider from "@/components/providers/modal-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
@@ -6,9 +7,11 @@ import { Toaster } from "sonner";
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
