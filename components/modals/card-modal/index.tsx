dac4248a42dad52fcf1acb5805c94d5e47c7bb09
Prophecyuntold <Aminurr999@gmail.com>
2024-03-24 22:22:41 +0000

    add description component to edit cards

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,6 +6,8 @@ import { useCardModal } from "@/hooks/use-card-modal";
 import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
+import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -24,19 +26,19 @@ export const CardModal = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
           <div className="col-span-3">
             <div className="w-full space-y-6">
-              {/* {!cardData ? (
-               <Description.Skeleton />
-             ) : (
-               <Description data={cardData} />
-             )}
-             {!auditLogsData ? (
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+              {/* {!auditLogsData ? (
                <Activity.Skeleton />
              ) : (
                <Activity items={auditLogsData} />
              )} */}
             </div>
           </div>
-          {/* {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />} */}
+          {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
         </div>
       </DialogContent>
     </Dialog>
