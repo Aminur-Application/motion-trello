e0e04aba4b0777fa87f75dd5a7d985e098103449
Prophecyuntold <Aminurr999@gmail.com>
2024-03-30 01:23:06 +0000

    create audit log components

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -8,6 +8,8 @@ import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
 import { Description } from "./description";
 import { Actions } from "./actions";
+import { AuditLog } from "@prisma/client";
+import { Activity } from "./activity";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -17,6 +19,11 @@ export const CardModal = () => {
   const { data: cardData } = useQuery<CardWithList>({
     queryKey: ["card", id],
     queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
+  const { data: auditLogsData } = useQuery<AuditLog[]>({
+    queryKey: ["card-logs", id],
+    queryFn: () => fetcher(`/api/cards/${id}/logs`),
   });
 
   return (
@@ -31,11 +38,11 @@ export const CardModal = () => {
               ) : (
                 <Description data={cardData} />
               )}
-              {/* {!auditLogsData ? (
-               <Activity.Skeleton />
-             ) : (
-               <Activity items={auditLogsData} />
-             )} */}
+              {!auditLogsData ? (
+                <Activity.Skeleton />
+              ) : (
+                <Activity items={auditLogsData} />
+              )}
             </div>
           </div>
           {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
